af0e2c79c5c0bbb796c22538209849022367ce90
Tigran Gabrielyan <tigrangab@gmail.com>
2014-04-23 19:48:33 -0700

    Changed app theme to Holo.Light Cleanup imports

--- src/com/gedutech/ridesyncer/MainActivity.java
+++ src/com/gedutech/ridesyncer/MainActivity.java
@@ -1,19 +1,12 @@
 package com.gedutech.ridesyncer;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-import org.json.JSONObject;
-
-import com.gedutech.ridesyncer.models.User;
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
+
+import com.gedutech.ridesyncer.models.User;
 
 public class MainActivity extends Activity {
 
